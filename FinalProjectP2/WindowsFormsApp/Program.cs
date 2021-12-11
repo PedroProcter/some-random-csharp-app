edddcbeff416b6c010a83fbe2f5b373cc42d9437
PedroProcter <OProcter89@gmail.com>
2021-12-10 20:12:49 -0400

    feat: Add Splash screen

--- FinalProjectP2/WindowsFormsApp/Program.cs
+++ FinalProjectP2/WindowsFormsApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp.subOptionsSistema;
 
 namespace WindowsFormsApp
 {
@@ -16,6 +17,7 @@ namespace WindowsFormsApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Splash());
             Application.Run(new Form1());
         }
     }
